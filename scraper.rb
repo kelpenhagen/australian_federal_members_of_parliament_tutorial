805f710289e69ddc3c74b1f60633ffd0d2c14db6
Henare Degan <henare.degan@gmail.com>
2015-09-04 12:13:19 +1000

    Loop through the 3 pages of results

--- scraper.rb
+++ scraper.rb
@@ -7,21 +7,23 @@ require 'mechanize'
 agent = Mechanize.new
 
 # Read in a page
-url = "http://www.aph.gov.au/Senators_and_Members/Parliamentarian_Search_Results?q=&mem=1&sen=1&par=-1&gen=0&ps=100&st=1"
+base_url = "http://www.aph.gov.au/Senators_and_Members/Parliamentarian_Search_Results?q=&mem=1&sen=1&par=-1&gen=0&ps=100&st=1&page="
 
-page = agent.get(url)
+["1", "2", "3"].each do |number|
+  page = agent.get(base_url + number)
 
-page.at(".search-filter-results").search(:li).each do |li|
-  dts = li.at(:dl).search(:dt)
+  page.at(".search-filter-results").search(:li).each do |li|
+    dts = li.at(:dl).search(:dt)
 
-  record = {
-    name: li.at(".title").inner_text,
-    party: dts.select { |dt| dt.inner_text == "Party" }.first.next.inner_text,
-    electorate: dts.select { |dt| dt.inner_text =~ /for/ }.first.next.next.inner_text
-  }
+    record = {
+      name: li.at(".title").inner_text,
+      party: dts.select { |dt| dt.inner_text == "Party" }.first.next.inner_text,
+      electorate: dts.select { |dt| dt.inner_text =~ /for/ }.first.next.next.inner_text
+    }
 
-  puts "Saving record for #{record[:name]}..."
-  ScraperWiki.save_sqlite([:name], record)
+    puts "Saving record for #{record[:name]}..."
+    ScraperWiki.save_sqlite([:name], record)
+  end
 end
 
 puts "Done."
